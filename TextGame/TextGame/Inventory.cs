8c4964246dc4a1376bfc224649cdc1a4e268501f
dbswls2114 <54023919+dbswls2114@users.noreply.github.com>
2023-11-12 17:47:23 +0900

    end

---
+++ TextGame/TextGame/Inventory.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextGame
+{
+    public enum ItemType
+    {
+        Weapon,
+        Armor
+    }
+    public class Item
+    {
+        public int id;
+        public string name;
+        public ItemType type;
+        public string description;
+
+    }
+    public class Weapon : Item
+    {
+        public int atk;
+        public Weapon(int _id, string _name, ItemType _type, int _atk, string _des)
+        {
+            id = _id;
+            name = _name;
+            type = _type;
+            atk = _atk;
+            description = _des;
+        }
+    }
+    public class Armor : Item
+    {
+        public int def;
+        public int hp;
+        public Armor(int _id, string _name, ItemType _type, int _def, int _hp, string _des)
+        {
+            id = _id;
+            name = _name;
+            type = _type;
+            def = _def;
+            hp = _hp;
+            description = _des;
+        }
+    }
+
+    internal class Inventory
+    {
+        public List<Weapon> weapons = new List<Weapon>();
+        public List<Armor> armors = new List<Armor>();
+        Weapon w1 = new Weapon(1, "롱소드", ItemType.Weapon, 5, "평범한 롱소드");
+        Weapon w2 = new Weapon(2,"목검", ItemType.Weapon, 1, "썩은 목검");
+        Armor a1 = new Armor(3,"사슬갑옷", ItemType.Armor, 2, 10, "사슬로 만든 갑옷");
+        Armor a2 = new Armor(4,"가죽옷", ItemType.Armor, 1, 5, "낡은 가죽옷");
+
+        public Inventory()
+        {
+            weapons.Add(w1);
+            weapons.Add(w2);
+            armors.Add(a1);
+            armors.Add(a2);
+        }
+
+        public void DisplayMyInventory()
+        {
+            Console.Clear();
+
+            Console.WriteLine("인벤토리");
+            Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.");
+            Console.WriteLine();
+            Console.WriteLine("[아이템 목록]");
+            Console.WriteLine("[무기]");
+            foreach (Weapon item in weapons)
+            {
+                Console.WriteLine($"{item.name} / {item.type} / {item.atk} / {item.description}");
+            }
+            Console.WriteLine("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+            Console.WriteLine("[방어구]");
+            foreach(Armor item in armors)
+            {
+                Console.WriteLine($"{item.name} / {item.type} / {item.def} / {item.hp} / {item.description}");
+            }
+            Console.WriteLine("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+            Console.WriteLine();
+            Console.WriteLine("0. 나가기");
+            Console.WriteLine("1. 장착관리");
+        }
+    }
+}
--- TextGame/TextGame/Program.cs
+++ TextGame/TextGame/Program.cs
@@ -5,10 +5,13 @@ namespace TextGame
     internal class Program
     {
         private static Character player;
+        private static Inventory inventory = new Inventory();
 
         static void Main()
         {
             GameDataSetting();
+            player.weapon = inventory.weapons[0];
+            player.armor = inventory.armors[0];           
             DisplayGameIntro();
         }
 
@@ -40,7 +43,7 @@ namespace TextGame
                     break;
 
                 case 2:
-                    // 작업해보기
+                    DisplayInventory();
                     break;
             }
         }
@@ -50,18 +53,18 @@ namespace TextGame
             Console.Clear();
 
             Console.WriteLine("상태보기");
-            Console.WriteLine("캐릭터의 정보르 표시합니다.");
+            Console.WriteLine("캐릭터의 정보를 표시합니다.");
             Console.WriteLine();
             Console.WriteLine($"Lv.{player.Level}");
             Console.WriteLine($"{player.Name}({player.Job})");
-            Console.WriteLine($"공격력 :{player.Atk}");
-            Console.WriteLine($"방어력 : {player.Def}");
-            Console.WriteLine($"체력 : {player.Hp}");
+            Console.WriteLine($"공격력 :{player.Atk} ({player.weapon.atk})");
+            Console.WriteLine($"방어력 : {player.Def} ({player.armor.def})");
+            Console.WriteLine($"체력 : {player.Hp} ({player.armor.hp})");
             Console.WriteLine($"Gold : {player.Gold} G");
             Console.WriteLine();
             Console.WriteLine("0. 나가기");
 
-            int input = CheckValidInput(0, 0);
+            int input = CheckValidInput(0,0);
             switch (input)
             {
                 case 0:
@@ -72,7 +75,72 @@ namespace TextGame
 
         static void DisplayInventory()
         {
+            inventory.DisplayMyInventory();
 
+            int input = CheckValidInput(0, 1);
+            switch (input)
+            {
+                case 0:
+                    DisplayGameIntro();
+                    break;
+                case 1:
+                    DisplaySetItem();                    
+                    break;
+            }
+        }
+        static void DisplaySetItem()
+        {
+            int itemNum = 1;
+            Console.WriteLine("[아이템 목록]");
+            Console.WriteLine("[무기]");
+            foreach (Weapon item in inventory.weapons)
+            {
+                if(player.weapon == item)
+                {
+                    Console.Write("[E]");
+                }
+                Console.WriteLine($"{itemNum}. {item.name} / {item.type} / {item.atk} / {item.description}");
+                itemNum++;
+            }
+            itemNum = 1;
+            Console.WriteLine("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+            Console.WriteLine("[방어구]");
+            foreach (Armor item in inventory.armors)
+            {
+                
+                if (player.armor == item)
+                {
+                    Console.Write("[E]");
+                }
+                Console.WriteLine($"{itemNum}. {item.name} / {item.type} / {item.def} / {item.hp} / {item.description}");
+                itemNum++;
+            }
+            Console.WriteLine("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+            Console.WriteLine("0. 나가기");
+            Console.WriteLine("1. 무기장착");
+            Console.WriteLine("2. 방어구장착");
+
+            int input = CheckValidInput(0, 2);
+            switch (input)
+            {
+                case 0:
+                    DisplayGameIntro();
+                    break;
+                case 1:
+                    Console.WriteLine("장착할 무기의 번호를 입력하세요");
+                    int winput = CheckValidInput(1, inventory.weapons.Count);
+                    player.weapon = inventory.weapons[winput-1];
+                    Console.WriteLine("장착완료");
+                    DisplayInventory();
+                    break;
+                case 2:
+                    Console.WriteLine("장착할 방어구의 번호를 입력하세요");
+                    int ainput = CheckValidInput(1, inventory.armors.Count);
+                    player.armor = inventory.armors[ainput];
+                    Console.WriteLine("장착완료");
+                    DisplayInventory();
+                    break;
+            }
         }
 
         static int CheckValidInput(int min, int max)
@@ -102,6 +170,8 @@ namespace TextGame
         public int Def { get; }
         public int Hp { get; }
         public int Gold { get; }
+        public Weapon weapon;
+        public Armor armor;
 
         public Character(string name, string job, int level, int atk, int def, int hp, int gold)
         {
@@ -114,3 +184,4 @@ namespace TextGame
             Gold = gold;
         }
     }
+}
